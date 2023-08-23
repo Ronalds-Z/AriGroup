3c69efa384b2db4f2c867ad140c4508ae50e1a2f
Ronalds-Z <ronaldsz123@inbox.lv>
2023-08-23 22:34:56 +0300

    header color change on scroll//comment fixes

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const headscroll = document.querySelector(".header");
-window.addEventListener("scroll", () => {
-    const pagescroll = window.scrollY || window.pageYOffset;
-    if (pagescroll > 1) {
-        headscroll.classList.add("scrolled");
-    }
-    else {
-        headscroll.classList.remove("scrolled");
-    }
-})
+const headerElement = document.querySelector(".header");
+if (headerElement) {
+    window.addEventListener("scroll", () => {
+        const pagescroll = window.scrollY;
+
+        if (pagescroll > 1) {
+            headerElement.classList.add("scrolled");
+        }
+        else {
+            headerElement.classList.remove("scrolled");
+        }
+    })
+}
