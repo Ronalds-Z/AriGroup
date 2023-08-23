3859bea678a194fb18bf22f426a057aa8d05a390
Ronalds-Z <ronaldsz123@inbox.lv>
2023-08-23 21:33:00 +0300

    header color change on scroll

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const burgeritem = document.querySelector(".burger-menu");
-
-if (burgeritem) {
-    burgeritem.addEventListener("click", () => {
-        alert("burger clicked")
-    })
-}
+const headscroll = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+    const pagescroll = window.scrollY || window.pageYOffset;
+    if (pagescroll > 1) {
+        headscroll.classList.add("scrolled");
+    }
+    else {
+        headscroll.classList.remove("scrolled");
+    }
+})
